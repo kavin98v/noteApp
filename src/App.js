700c0e2ab30426bdd73138c846029bd82cf7fa52
kavindu <shalindaworks98@gmail.com>
2024-01-25 16:55:18 +0530

    add colors

--- src/App.js
+++ src/App.js
@@ -1,57 +1,65 @@
 import { useState } from "react";
 import "./App.css";
 import AddNote from "./Components/AddNote";
+import NavBar from "./Components/NavBar";
 
 function App() {
   const [noteList, setNoteList] = useState([
-    "Do Honework",
-    "go to gym",
-    "hav lunch",
+    { note: "Do Honework", user: "Kavindu" },
+    { note: "go to gym", user: "holin" },
+    { note: "hav lunch", user: "Kavindu" },
   ]);
 
-  console.log(noteList);
-
-  function stringHandle(text) {
-    const newNoteList = [text, ...noteList];
+  function stringHandle(obj) {
+    const newNoteList = [obj, ...noteList];
 
     setNoteList(newNoteList);
   }
 
-  function handleDelete(indexToDelete){
+  function handleDelete(indexToDelete) {
     // Create a new array without the item at the specified index
-    const updatedNoteList = noteList.filter((_, index) => index !== indexToDelete);
+    const updatedNoteList = noteList.filter(
+      (_, index) => index !== indexToDelete
+    );
     setNoteList(updatedNoteList);
   }
 
-  
-
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="col-span-2 gap-2 flex flex-col">
-        {noteList.map((item, index) => (
-          <div className="border border-gray-900 flex justify-between" key={item}>
-            <p>{item}</p>
-            <button onClick={()=>handleDelete(index) }>
-              <svg
-                class="w-6 h-6 text-red-400 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18 18 6m0 12L6 6"
-                />
-              </svg>
-            </button>
-          </div>
-        ))}
-      </div>
-      <div>
+    <div className="bg-slate-900 h-screen">
+      <NavBar />
+      <div className="grid grid-cols-3 gap-4 p-8">
+        <div className="col-span-2 gap-2 flex flex-col">
+          {noteList.map((item, index) => (
+            <div
+              className="border border-gray-900 flex flex-col bg-slate-500 p-2"
+              key={index}
+            >
+              <div className=" flex justify-between">
+                <p className="text text-sm text">{item.user}</p>
+                <button onClick={() => handleDelete(index)}>
+                  <svg
+                    className="w-6 h-6 text-red-400 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18 18 6m0 12L6 6"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <p className="text-lg truncate">{item.note}</p>
+              </div>
+            </div>
+          ))}
+        </div>
         <AddNote addnote={stringHandle} />
       </div>
     </div>
